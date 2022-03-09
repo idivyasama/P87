6b595f57aa795056bbc533532c546bf704e55c4a
idivyasama <96538739+idivyasama@users.noreply.github.com>
2022-03-08 20:28:35 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+var canvas=new fabric.Canvas('MyCanvas');
+// Create canvas variable
+ block_y=1;
+ block_x=1;
+
+block_image_width = 350;
+block_image_height = 430;
+
+var block_image_object= "";
+
+function new_image(get_image)
+{
+		block_image_object=Img;
+		
+		block_image_object.scaleToWidth( block_image_width);
+		block_image_object.scaleToheight( block_image_height);
+		block_image_object.set({
+		top:player_y,
+		left:player_x
+		});
+		canvas.add( block_image_object);
+		
+		
+		};
+		
+	// to upload images
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+
+	if(keyPressed == '82') // add appropriate keycode
+	{
+		new_image('rr1.png')
+		console.log('r');
+		// upload red ranger
+	}
+	if(keyPressed == '71')
+	{
+		new_image('gr.png')
+		console.log('g');
+		block_x = 200;
+		// upload green ranger
+	}
+	
+	if(keyPressed == '89')
+	{
+		new_image('yr.png')
+		console.log('y');
+		block_x =350;
+		// upload yellow ranger
+	}
+	if(keyPressed == '80')
+	{
+		new_image('pr.png')
+		console.log('p');
+		block_x = 600;
+		// upload pink ranger
+	}
+	if(keyPressed == '66')
+	{
+		new_image('br.png')
+		console.log('b');
+		block_x = 700;
+	// upload blue ranger
+	}
+	
+}
+
